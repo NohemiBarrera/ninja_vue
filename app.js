391f5e50b4da20e94bd445d14c2689eeeb47f3f1
Nohemi Barrera <nohemi.baol@gmail.com>
2017-11-30 18:58:23 -0600

    something

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 new Vue({	//vue instance
 	el:'#vue-app',	//this is what the instance gonna control
 	data: {
-		name: 'Meme',
-		job: 'Developer',
-		website: 'hhtp://www.thenetninja.co.uk',
-		websiteTag: '<a href="hhtp://www.thenetninja.co.uk">Ninja net</a>'
+		age: 23,
+		x: 0,
+		y: 0
 	},
 	methods: {
-		greet: function(time){
-			return 'Good ' + time + ' ' + this.name;
+		add: function(inc){
+			this.age += inc;
+		},
+		subtract: function(dec){
+			this.age-=dec;
+		},
+		updateXY:function(event){
+			this.x = event.offsetX;
+			this.y = event.offsetY;
 		}
 	}
 }); 
